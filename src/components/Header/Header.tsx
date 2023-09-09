bde4a6d79011fdf5146450c0560f8ba0ef87b69a
Nathalie Zambrano <nathalievzm@gmail.com>
2023-09-09 17:27:35 -0500

    feat: add function to toggle navbar on mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import './Header.sass'
 import { useContext } from 'react'
-import { MagnifyingGlass, FilmSlate } from '@phosphor-icons/react'
+import { MagnifyingGlass, FilmSlate, List } from '@phosphor-icons/react'
 import { SearchContext } from '../../core/context'
+import { Button } from '..'
 
 export const Header = () => {
     const { search, setSearch } = useContext(SearchContext)
@@ -10,11 +11,29 @@ export const Header = () => {
         setSearch(event.target.value)
     }
 
+    const toggleNavbar = () => {
+        const navbar = document.querySelector('#aside')
+        navbar?.classList.toggle('show')
+    }
+
     return (
         <header id='header'>
             <section className='logo'>
-                <FilmSlate size={24} color='#ff4ab0' weight="duotone" />
-                <h1>CINEMAFUN</h1>
+                <div>
+                    <FilmSlate size={24} color='#ff4ab0' weight="duotone" />
+                    <h1>CINEMAFUN</h1>
+                </div>
+
+                {/* <button id="toggleNavbarButton" className="button">
+                    <List size={16} color='#0f0f15' weight='duotone' />
+                </button> */}
+
+                <Button
+                    label=''
+                    icon={<List size={16} color='#0f0f15' weight='duotone' />}
+                    showIcon={true}
+                    onClickFunction={toggleNavbar}
+                />
             </section>
             <section className='input'>
                 <div><MagnifyingGlass size={16} weight="duotone" /></div>
